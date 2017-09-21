f61456a3fcf4a89557b0f65a70b8e71c5554e0b5
t00037790 <izzymovil@gmail.com>
2017-09-21 14:48:32 -0500

    mysql, redis configuration

---
+++ ansible002/clean_all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "Detediendo dockers"
+docker stop server01 server02 server03
+echo "Dockers detenidos"
+echo
+echo "----------------------"
+echo "Eliminando dockers"
+docker rm server01 server02 server03
+echo "Dockers eliminados."
+echo
+echo "----------------------"
+echo "Eliminando configuración residual en ssh"
+#for i in `seq 1 3`;
+#do
+sed -i '' "/server01/d" $HOME/.ssh/known_hosts
+#  #ssh-keyscan -H server0$i -p 222$i >> $HOME/.ssh/known_hosts
+
+echo "Limpieza lista."
+echo
+echo "----------------------"
+echo "Recreando dockers"
+./create_dockers.sh server:16.04
+
+ssh -o StrictHostKeyChecking=no root@server01 -p 2221 -i key.private hostname
+echo "Adicionadas las llaves ssh."
+echo
+echo "----------------------"
+echo "Sigue con ansible001"
