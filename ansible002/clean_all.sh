0f13fc131b6712836312490935277151d8d16f5e
t00037790 <izzymovil@gmail.com>
2017-09-29 15:04:17 -0500

    nginx role added

--- ansible002/clean_all.sh
+++ ansible002/clean_all.sh
@@ -10,7 +10,7 @@ echo "Dockers eliminados."
 echo
 echo "----------------------"
 echo "Eliminando configuración residual en ssh"
-sed -i '' "/server01/d" $HOME/.ssh/known_hosts
+sed -i '' "/server01/d" /root/.ssh/known_hosts
 #  #ssh-keyscan -H server0$i -p 222$i >> $HOME/.ssh/known_hosts
 
 echo "Limpieza lista."
